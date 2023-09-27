cc827e95df850a7b35fcbd70b7895fb17dcad25c
simatosc <costas.simatos@sigmacubed.eu>
2023-09-27 11:15:15 +0200

    Updates for release 1.21.1.

--- src/main/resources/archetype-resources/src/main/java/gitb/ProcessingServiceConfig.java
+++ src/main/resources/archetype-resources/src/main/java/gitb/ProcessingServiceConfig.java
@@ -2,12 +2,10 @@ package ${package}.gitb;
 
 import org.apache.cxf.Bus;
 import org.apache.cxf.jaxws.EndpointImpl;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.xml.namespace.QName;
-import javax.xml.ws.Endpoint;
 
 /**
  * Configuration class responsible for creating the Spring beans required by the service.
@@ -15,19 +13,13 @@ import javax.xml.ws.Endpoint;
 @Configuration
 public class ProcessingServiceConfig {
 
-    @Autowired
-    private Bus cxfBus = null;
-
-    @Autowired
-    private ProcessingServiceImpl processingServiceImplementation = null;
-
     /**
      * The CXF endpoint that will serve service calls.
      *
      * @return The endpoint.
      */
     @Bean
-    public Endpoint processingService() {
+    public EndpointImpl processingService(Bus cxfBus, ProcessingServiceImpl processingServiceImplementation) {
         EndpointImpl endpoint = new EndpointImpl(cxfBus, processingServiceImplementation);
         endpoint.setServiceName(new QName("http://www.gitb.com/ps/v1/", "ProcessingServiceService"));
         endpoint.setEndpointName(new QName("http://www.gitb.com/ps/v1/", "ProcessingServicePort"));
